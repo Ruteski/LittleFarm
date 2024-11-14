25783a0d13d113d7c24c35f4cfa755a2577112bc
Lincoln Ruteski <lruteski@gmail.com>
2024-11-13 21:10:34 -0300

    Player
    criado player com movimentacao, criado animacao de idle e walk

---
+++ scripts/player/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export_category("Variaveis")
+@export var _move_speed: float = 75.0
+
+@export_category("Objetos")
+@export var _animation: AnimationPlayer
+
+
+func _physics_process(delta: float) -> void:
+	move()
+	animate()
+	
+func move() -> void:
+	# o get_vector automagicamente normaliza o vetor
+	var _direction: Vector2 = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+	
+	velocity = _direction * _move_speed
+	move_and_slide()
+
+
+func animate() -> void:
+	if velocity:
+		if velocity.x > 0:
+			_animation.play("walk_right")
+		else:
+			_animation.play("walk_left")
+	else:
+		_animation.play("idle")
